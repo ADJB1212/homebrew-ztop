e0532a8debc8c5132453d00978769ca4713f5c72
Andrew Jaffe <adjaffeb@gmail.com>
2026-04-28 21:06:31 -0400

    ztop 0.0.5

--- Formula/ztop.rb
+++ Formula/ztop.rb
@@ -1,28 +1,28 @@
 class Ztop < Formula
   desc "Terminal-based system monitor for macOS and Linux"
   homepage "https://github.com/ADJB1212/ztop"
-  version "0.0.4"
+  version "0.0.5"
   license "GPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://github.com/ADJB1212/ztop/releases/download/v0.0.4/ztop-0.0.4-macos-arm64.tar.gz"
-      sha256 "42452b723602e30d7820c8d3d734f22ecdd4ea7a12f289603a957b9111733d49"
+      url "https://github.com/ADJB1212/ztop/releases/download/v0.0.5/ztop-0.0.5-macos-arm64.tar.gz"
+      sha256 "749627001862e0b8f2fd3326427686aec0d9ddba1caedeec5ee0c2199fa0bdbd"
     end
     on_intel do
-      url "https://github.com/ADJB1212/ztop/releases/download/v0.0.4/ztop-0.0.4-macos-x86_64.tar.gz"
-      sha256 "bcdd20725e3533d41b90eb1c4489deba0c44acb1222ef54bade2fb9edf65d34d"
+      url "https://github.com/ADJB1212/ztop/releases/download/v0.0.5/ztop-0.0.5-macos-x86_64.tar.gz"
+      sha256 "373aae6fa4d60608c3507acabdfe3f63be13c76acd0602c78fcf763c1d6d9eeb"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ADJB1212/ztop/releases/download/v0.0.4/ztop-0.0.4-linux-arm64.tar.gz"
-      sha256 "27a3ca94d65cbb8c9e09e3239732fe725f067c8374e57226d1c2ed77fc592800"
+      url "https://github.com/ADJB1212/ztop/releases/download/v0.0.5/ztop-0.0.5-linux-arm64.tar.gz"
+      sha256 "96ca03d97c3c1cef1ab3f28745ec47d3a778d5e21b97c0333eedf4658009cca6"
     end
     on_intel do
-      url "https://github.com/ADJB1212/ztop/releases/download/v0.0.4/ztop-0.0.4-linux-x86_64.tar.gz"
-      sha256 "662f27583c37bbc78fcf2e013418dc2c3fdd8b1aa13479b78beb77f488580626"
+      url "https://github.com/ADJB1212/ztop/releases/download/v0.0.5/ztop-0.0.5-linux-x86_64.tar.gz"
+      sha256 "9f313bff97107a39b3db4ecaefefce1409c827794f8e829ab94c58202a26bd07"
     end
   end
 
