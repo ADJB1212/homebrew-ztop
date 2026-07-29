8c4d66e3da358175fd338a8fbbff23246e798c2b
Andrew Jaffe <adjaffeb@gmail.com>
2026-07-29 11:28:13 -0400

    ztop 1.1.0

--- Formula/ztop.rb
+++ Formula/ztop.rb
@@ -1,13 +1,13 @@
 class Ztop < Formula
   desc "Terminal-based system monitor for macOS"
   homepage "https://github.com/ADJB1212/ztop"
-  version "1.0.0"
+  version "1.1.0"
   license "GPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://github.com/ADJB1212/ztop/releases/download/v1.0.0/ztop-1.0.0-macos-arm64.tar.gz"
-      sha256 "a4499bceac2bea739d9d026879ce9fc7aa8d844ddb2654caf439e7c0263be96a"
+      url "https://github.com/ADJB1212/ztop/releases/download/v1.1.0/ztop-1.1.0-macos-arm64.tar.gz"
+      sha256 "c236245291ed3f7a4cd092ee0cc263011f23454ed1b0e0e07c4c378f4d1e5180"
     end
   end
 
